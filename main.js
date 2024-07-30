ded0d9e782e14c8750132c0f47d629ace016429a
dinaragayratova01 <151494786+dinaragayratova01@users.noreply.github.com>
2024-07-30 11:04:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// for (let i = 20; i >= 10; i--) {
+//     console.log(i);
+// }
+// let i = 0
+// while (i < 10) {
+//     console.log(i);
+//     i++
+// }
+// let i = 0
+// do {
+//     console.log("hello");
+//     i++
+// } while (i < 5)
+
+
+// for (let i = 0; i < 10; i++) {
+//    if (1 % 2 == 0) {
+//     console.log(i);
+//    }
+// }
+    
+let sum = 0
+for (let i = 0; i <= 10; i++) {
+   sum = sum + 1
+}
+console.log(sum);  
+ 
